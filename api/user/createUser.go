912e5e096e90dac6f079e1f1dd9064a2b18a9a95
george <george@George.GeOrGe>
2025-02-14 21:37:33 +0000

    add CreateUser.go

--- api/user/createUser.go
+++ api/user/createUser.go
@@ -1,7 +1,31 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/geoo115/property-manager/db"
+	"github.com/geoo115/property-manager/models"
+	"github.com/geoo115/property-manager/utils"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
+	var user models.User
 
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		return
+	}
+	HashPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error hashing password"})
+		return
+	}
+	user.Password = HashPassword
+
+	if err := db.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error creating User"})
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
