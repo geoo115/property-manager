8739d559779329d691ec1de9d2d0657159c610e9
george <george@George.GeOrGe>
2025-02-16 14:35:28 +0000

    update:add cors configure

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/geoo115/property-manager/db"
 	"github.com/geoo115/property-manager/router"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,7 +15,17 @@ func main() {
 	db.Init()
 	r := gin.Default()
 
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	router.SetupRouter(r)
+
 	fmt.Println("Server is running at 8080")
 	log.Fatal(r.Run(":8080"))
 }
