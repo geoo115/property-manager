8274ad21794025ea8621a4ad928446879024c64b
george <george@George.GeOrGe>
2025-02-18 14:23:13 +0000

    add:maintenance router

--- router/maintenance_router.go
+++ router/maintenance_router.go
@@ -1,1 +1,14 @@
 package router
+
+import (
+	"github.com/geoo115/property-manager/api/maintenance"
+	"github.com/gin-gonic/gin"
+)
+
+func MaintenanceRoutes(rg *gin.RouterGroup) {
+	rg.GET("/maintenances", maintenance.GetMaintenances)
+	rg.GET("/maintenance/:id", maintenance.GetMaintenance)
+	rg.POST("/maintenance", maintenance.CreateMaintenance)
+	rg.PUT("/maintenance/:id", maintenance.UpdateMaintenance)
+	rg.DELETE("/maintenance/:id", maintenance.DeleteMaintenance)
+}
--- router/router.go
+++ router/router.go
@@ -2,7 +2,9 @@ package router
 
 import (
 	"github.com/geoo115/property-manager/api/lease"
+	"github.com/geoo115/property-manager/api/maintenance"
 	"github.com/geoo115/property-manager/api/property"
+	"github.com/geoo115/property-manager/api/user"
 	"github.com/geoo115/property-manager/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +20,7 @@ func SetupRouter(r *gin.Engine) {
 		UserRouter(admin)
 		PropertyRouter(admin)
 		LeaseRouter(admin)
+		MaintenanceRoutes(admin)
 	}
 
 	// Landlord group: restricted access to their properties and leases.
@@ -28,6 +31,8 @@ func SetupRouter(r *gin.Engine) {
 		landlord.GET("/properties/:id", property.GetPropertyByID)
 		landlord.GET("/leases", lease.GetLeases)
 		landlord.GET("/leases/:id", lease.GetLeaseByID)
+		landlord.GET("/properties/:id/maintenances", maintenance.GetLandlordMaintenances)
+		landlord.POST("/properties/:id/maintenances", maintenance.CreateLandlordMaintenance)
 	}
 
 	// Tenant group: can only access their leases.
@@ -36,5 +41,17 @@ func SetupRouter(r *gin.Engine) {
 	{
 		tenant.GET("/leases", lease.GetLeases)
 		tenant.GET("/leases/:id", lease.GetLeaseByID)
+		tenant.GET("/leases/:id/maintenance", maintenance.GetMaintenances)
+		tenant.POST("/leases/:id/maintenance", maintenance.CreateMaintenance)
+	}
+
+	maintenanceTeam := r.Group("/maintenanceTeam")
+	maintenanceTeam.Use(middleware.JWTMiddleware(), middleware.RoleMiddleware("maintenanceTeam"))
+	{
+		maintenanceTeam.GET("/maintenances", maintenance.GetMaintenances)
+		maintenanceTeam.GET("/maintenance/:id", maintenance.GetMaintenance)
+		maintenanceTeam.PUT("/maintenance/:id", maintenance.UpdateMaintenance)
+		maintenanceTeam.GET("/users", user.GetUsers)
+		maintenanceTeam.GET("/properties", property.GetProperties)
 	}
 }
